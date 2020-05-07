f4b547ec3d11e66559649af219fca42d1200e521
Pranav Kapoor <pranavkapoor001@gmail.com>
2020-05-08 02:35:26 +0530

    X00TD: Build Thermal packages
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- device.mk
+++ device.mk
@@ -263,6 +263,10 @@ PRODUCT_COPY_FILES += \
     
 # Soong
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
+
+# Thermal
+PRODUCT_PACKAGES += \
+    thermal.sdm660
     
 # Touchscreen
 PRODUCT_PACKAGES += \
