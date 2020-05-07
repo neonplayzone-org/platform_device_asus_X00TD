6864b9149338650837f556c847104addfcfc67cb
Pranav Kapoor <pranavkapoor001@gmail.com>
2020-05-08 00:44:25 +0530

    X00TD: Build Renderscript HIDL
    Signed-off-by: Pranav Kapoor <pranavkapoor001@gmail.com>
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -69,6 +69,10 @@ PRODUCT_PACKAGES += \
 # Netutils
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.0
+    
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
 
 # RIL
 PRODUCT_PACKAGES += \
