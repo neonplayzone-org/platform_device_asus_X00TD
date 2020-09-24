c15c98b074c6c4759000c0552708e39cca38c864
Anver sadhique <akurun@codeaurora.org>
2020-09-24 17:55:58 +0530

    X00TD: Set higher priority to c2 than OMX
    Change-Id: I049a2ca92295d53abee0c95f637414d479775334
    Signed-off-by: Anver sadhique <akurun@codeaurora.org>
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -132,7 +132,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
-    debug.stagefright.omx_default_rank.sw-audio=1 \
     debug.stagefright.omx_default_rank=0 \
     vendor.vidc.enc.disable.pq=true \
     vendor.vidc.dec.enable.downscalar=0
